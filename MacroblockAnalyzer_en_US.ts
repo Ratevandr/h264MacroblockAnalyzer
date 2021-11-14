e356c4008ec51ce798c38e8911e3e1efd3cf489a
Landgraf <Landgraf132@yandex.ru>
2021-11-15 00:14:26 +0300

    updated screenshot

---
+++ MacroblockAnalyzer_en_US.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="14"/>
+        <source>Macroblock analyzer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="146"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="83"/>
+        <source>▶</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="141"/>
+        <source>Show frame number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="116"/>
+        <source>00:00:01:20</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="135"/>
+        <source>GoToFrame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="270"/>
+        <source>Frame type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="276"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="94"/>
+        <source>⏩️</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="76"/>
+        <source>⏪</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="104"/>
+        <source>⏸</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="64"/>
+        <source>📂</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="146"/>
+        <source>Show Mb SKIP</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
